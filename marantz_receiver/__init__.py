513466e6226d2723dd30c0f625a8614dcb55e332
Andrew Carey <andrew@Andrews-MacBook-Pro.local>
2018-01-13 19:51:09 +1100

    updates to filter out bad results and fix volume/source mapping

--- marantz_receiver/__init__.py
+++ marantz_receiver/__init__.py
@@ -12,10 +12,12 @@ from marantz_receiver.marantz_commands import CMDS
 import serial  # pylint: disable=import-error
 import threading
 import telnetlib
+import logging
 
 DEFAULT_TIMEOUT = 1
 DEFAULT_WRITE_TIMEOUT = 1
 
+_LOGGER = logging.getLogger(__name__)
 
 class MarantzReceiver(object):
     """Marantz receiver."""
@@ -33,6 +35,7 @@ class MarantzReceiver(object):
 
         The receiver will always return a value, also when setting a value.
         """
+        raw_command = CMDS[domain][function]['cmd']
         if operator in CMDS[domain][function]['supported_operators']:
             if operator is ':' and value is None:
                 raise ValueError('No value provided')
@@ -53,15 +56,22 @@ class MarantzReceiver(object):
         self.lock.acquire()
 
         final_command = ''.join(['@', cmd, '\r']).encode('utf-8')
+        _LOGGER.info ('Send Command %s',final_command)
         self.ser.write(final_command)
 
         # Marantz uses the prefix @ and the suffix \r.
-        # With this we read the first \r and skip it
         msg = self.ser.read_until(bytes('\r'.encode()))
         self.lock.release()
-        print (msg.decode())
-        return msg.decode().strip().split(':')[1]
-        # b'AMT:0\r will return 0
+        _LOGGER.info ('Response %s', msg.decode())
+
+        split_string = msg.decode().strip().split(':')
+        _LOGGER.info("Split String %s", split_string)
+        _LOGGER.info ("Command: %s", raw_command)
+        if split_string[0] != ('@' + raw_command):
+            return None
+        else:
+             return split_string[1]
+             # b'AMT:0\r will return 0
 
     def main_mute(self, operator, value=None):
         """Execute Main.Mute."""
@@ -77,7 +87,9 @@ class MarantzReceiver(object):
 
         Returns int
         """
-        return int(self.exec_command('main', 'volume', operator, value))
+        vol_result = self.exec_command('main', 'volume', operator, value)
+        if vol_result != None:
+            return int(self.exec_command('main', 'volume', operator, value))
 
     def main_source(self, operator, value=None):
         """
@@ -85,7 +97,12 @@ class MarantzReceiver(object):
 
         Returns int
         """
-        return self.exec_command('main', 'source', operator, value)
+        result = self.exec_command('main', 'source', operator, value)
+        if result != None and len(result) == 2:
+            _LOGGER.info("Source Result: %s", result[1])
+            return result[1]
+        else:
+            return result
 
     def main_autostatus (self, operator, value=None):
         """
